dfab9b988c554a2119f5a8af88e9a789094b2fe3
Atharva Gokhale <atharvagokhale95@gmailcom>
2026-02-24 18:34:51 -0500

    Updates test.py file

--- README.md
+++ README.md
@@ -1,12 +1,23 @@
+# Python API
+
 Flask and FastAPI are Python Web Frameworks used to build APIs.
 
+## Drawbacks with Flask:
+
+1. Slower than FastAPI
+2. Less efficient with async programming
+3. No built-in data validation
+4. No automatic documentation generation
+
 REST (Representational State Transfer) HTTP Methods:
+
 1. GET → Retrieve Data
 2. POST → Create Data
 3. PUT → Update Data
 4. DELETE → Remove Data
 
 Example:
+
 1. Get list of Users -> GET
 2. Create a new User -> POST
 3. Update User info -> PUT
--- test.py
+++ test.py
@@ -1,7 +1,33 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
-@app.get("/hello")
-async def hello():
-    return {"message": "Hello, World!"}
+class AvailableCuisine(str, Enum):
+    indian = "indian"
+    italian = "italian"
+    american = "american"
+    
+Cuisine = {
+    AvailableCuisine.indian: ["Butter Chicken", "Naan", "Biryani"],
+    AvailableCuisine.italian: ["Pizza", "Pasta", "Tiramisu"],
+    AvailableCuisine.american: ["Hamburger", "Fries", "Apple Pie"]
+}
+
+food_items = {
+    "indian": ["Butter Chicken", "Naan", "Biryani"],
+    "italian": ["Pizza", "Pasta", "Tiramisu"],
+    "american": ["Hamburger", "Fries", "Apple Pie"]
+}   
+
+# This function is for demonstrating the advantage of FastAPI's automatic validation and documentation generation when using Enums.
+@app.get("/get_items/{cuisine}")
+async def get_items(cuisine):
+    return food_items.get(cuisine, "Cuisine not found")
+
+
+# @app.get("/get_items/{cuisine}")
+# async def get_items(cuisine: AvailableCuisine): # The parameter 'cuisine' is of type 'AvailableCuisine', which is an Enum (fixed set of constant values). FastAPI will automatically validate the input against the Enum values.
+#     return {"cuisine": cuisine.value, # The 'value' attribute of the Enum member will give the actual string value (e.g., "indian", "italian", "american") instead of the Enum member name (e.g., AvailableCuisine.indian).
+#         "dishes": Cuisine[cuisine]}
+
